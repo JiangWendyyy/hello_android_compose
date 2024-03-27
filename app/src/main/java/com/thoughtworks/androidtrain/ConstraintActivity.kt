d36c539470830812d7e28597957581f3a6346c49
JiangWendyyy <j16698507162@163.com>
2024-03-27 19:30:02 +0800

    add constraint_layout

--- app/src/main/java/com/thoughtworks/androidtrain/ConstraintActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/ConstraintActivity.kt
@@ -1,33 +1,20 @@
 package com.thoughtworks.androidtrain
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class ConstraintActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.constraint_layout)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        var button1 = findViewById<Button>(R.id.toConstraintActivity)
-        button1.setOnClickListener{
-            navigateToConstraintActivity()
-        }
     }
-
-    private fun navigateToConstraintActivity() {
-        val intent = Intent(this, ConstraintActivity::class.java)
-        startActivity(intent)
-    }
-
 }
