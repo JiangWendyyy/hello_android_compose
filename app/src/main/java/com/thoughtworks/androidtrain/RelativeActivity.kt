5630a0ee76608a711e49586289a4ec237c99c892
JiangWendyyy <j16698507162@163.com>
2024-03-27 17:11:16 +0800

    add relative_layout.xml and RelativeActivity

---
+++ app/src/main/java/com/thoughtworks/androidtrain/RelativeActivity.kt
@@ -0,0 +1,15 @@
+package com.thoughtworks.androidtrain
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RelativeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.relative_layout)
+    }
+}
