5e0728ad91f9e3bfd6ee93efcd32f613e16c4d22
JiangWendyyy <j16698507162@163.com>
2024-03-31 15:29:43 +0800

    get phone number from contacts and refactor MainActivity

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -6,16 +6,13 @@ import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.util.Log
-import android.view.KeyEvent
+import android.provider.ContactsContract.CommonDataKinds.Phone
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,22 +28,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        var button1 = findViewById<Button>(R.id.toConstraintActivity)
+        val button1 = findViewById<Button>(R.id.toConstraintActivity)
         button1.setOnClickListener {
             navigateToActivity(ConstraintActivity::class.java)
         }
-        var button2 = findViewById<Button>(R.id.toLoginActivity)
+        val button2 = findViewById<Button>(R.id.toLoginActivity)
         button2.setOnClickListener {
             navigateToActivity(LoginActivity::class.java)
         }
-        var button3 = findViewById<Button>(R.id.toContactActivity)
+        val button3 = findViewById<Button>(R.id.toContactActivity)
         button3.setOnClickListener {
-            //navigateToActivity(ContactActivity::class.java)
             selectContact()
-        }
-        var button4 = findViewById<Button>(R.id.toLanguageSectionActivity)
-        button4.setOnClickListener {
-            navigateToActivity(LanguageSelectionActivity::class.java)
         }
 
 
@@ -58,33 +50,58 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_SELECT_PHONE_NUMBER && resultCode == Activity.RESULT_OK) {
-            // Get the URI and query the content provider for the phone number.
+            // Get the URI and query the content provider for the contact
             val contactUri: Uri? = data?.data
             contactUri?.let {
-                val projection: Array<String> =
-                    arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-                val cursor = contentResolver.query(contactUri, projection, null, null, null)
-                // If the cursor returned is valid, get the phone number.
-                if (cursor != null) {
+                var name = ""
+                var phoneNumber: String? = null
+                val cursor = contentResolver.query(contactUri, null, null, null, null)
+                cursor?.use {
                     if (cursor.moveToFirst()) {
-                        val nameIndex =
-                            cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
-                        val name = nameIndex.let { cursor.getString(it) }
-/*                        val numberIndex =
-                            cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
-                        val number = numberIndex.let { cursor.getString(it) }*/
-                        // Do something with the phone number.
-                        Toast.makeText(
-                            this,
-                            "Selected Contact: $name, ", +
-        //                            "Phone: $number",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        cursor.close()
+                        val columnIndex =
+                            cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)
+                        val hasPhoneNumber =
+                            Integer.parseInt(columnIndex.let { cursor.getString(it) })
+                        // If the cursor returned is valid, get the phone number.
+                        if (hasPhoneNumber > 0) {
+                            phoneNumber = getPhoneNumberByCursorWithId(cursor)
+                        }
+                        name = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME).let { cursor.getString(it) }
                     }
                 }
+                showToast(name, phoneNumber)
             }
         }
+    }
+
+    private fun showToast(name: String, phoneNumber: String?) {
+        Toast.makeText(
+            this,
+            "Selected Contact name:$name, " +
+                    "phone: $phoneNumber, ",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun getPhoneNumberByCursorWithId(cursor: Cursor): String? {
+        val contactIdIndex =
+            cursor.getColumnIndex(ContactsContract.Contacts._ID)
+        val contactId = contactIdIndex.let { cursor.getString(it) }
+        val phoneCursor = contentResolver.query(
+            Phone.CONTENT_URI,
+            null,
+            Phone.CONTACT_ID + "=" + contactId,
+            null,
+            null
+        )
+        phoneCursor?.use {
+            while (phoneCursor.moveToNext()) {
+                val phoneIndex =
+                    phoneCursor.getColumnIndex(Phone.NUMBER)
+                return phoneIndex.let { phoneCursor.getString(it) }
+            }
+        }
+        return null
     }
 
     private fun selectContact() {
