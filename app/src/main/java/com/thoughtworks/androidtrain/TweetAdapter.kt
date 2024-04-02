5dc15d224832e8e16e1c433ecd35391340d38985
JiangWendyyy <j16698507162@163.com>
2024-04-02 17:01:46 +0800

    fix bug for item_bottom_button

--- app/src/main/java/com/thoughtworks/androidtrain/TweetAdapter.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetAdapter.kt
@@ -1,22 +1,36 @@
 package com.thoughtworks.androidtrain
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.thoughtworks.androidtrain.model.Tweet
+import kotlin.math.log
 
 class TweetAdapter (private val tweets: List<Tweet>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     class CommentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nickTextView: TextView = itemView.findViewById(R.id.nick)
         val contentTextView: TextView = itemView.findViewById(R.id.content)
+        val avatarImageView: ImageView = itemView.findViewById(R.id.image)
     }
 
     class FooterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val buttonReachedEnd: Button = itemView.findViewById(R.id.buttonReachedEnd)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if(position<tweets.size){
+            TYPE_COMMENT
+        }else{
+            TYPE_FOOTER
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -36,11 +50,21 @@ class TweetAdapter (private val tweets: List<Tweet>) : RecyclerView.Adapter<Recy
         if (holder is CommentViewHolder) {
             // 绑定评论数据
             val tweet = tweets[position]
+            Log.d("position", "CommentViewHolder position: $position: ")
             holder.nickTextView.text = tweet.sender.nick
             holder.contentTextView.text = tweet.content
-        } else if (holder is FooterViewHolder && position == tweets.size) {
-            // 显示底部按钮
-            holder.buttonReachedEnd.visibility = View.VISIBLE
+            holder.avatarImageView.load(tweet.sender.avatar) {
+                crossfade(true)
+                placeholder(R.drawable.avatar)
+                transformations(CircleCropTransformation())
+            }
+        } else if (holder is FooterViewHolder ) {
+            Log.d("FooterViewHolder", "is FooterViewHolder")
+            if (position == tweets.size) {
+                // 显示底部按钮
+                Log.d("position", "FooterViewHolder position: ${tweets.size} ")
+                holder.buttonReachedEnd.visibility = View.VISIBLE
+            }
         }
     }
 
--- app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.androidtrain
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,10 +10,9 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.thoughtworks.androidtrain.model.Tweet
 
-class TweetsActivity : AppCompatActivity() {
+class TweetsActivity : AppCompatActivity(R.layout.tweets_layout) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tweets_layout)
 
         val tweetList = deserializeFromJson<List<Tweet>>(JsonConstants.feedList)
         val filteredTweets =
@@ -27,9 +27,9 @@ class TweetsActivity : AppCompatActivity() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-                val isLastItemVisible = layoutManager.findLastVisibleItemPosition() == filteredTweets.size - 1
-                val isAtBottom = dy > 0 && isLastItemVisible
                 val footerViewHolder = recyclerView.findViewHolderForAdapterPosition(filteredTweets.size) as? TweetAdapter.FooterViewHolder
+                val isLastItemVisible = layoutManager.findLastVisibleItemPosition() >= filteredTweets.size
+                val isAtBottom = dy > 0 && isLastItemVisible
                 if (isAtBottom && footerViewHolder != null) {
                     footerViewHolder.buttonReachedEnd.visibility = View.VISIBLE
                 } else {
