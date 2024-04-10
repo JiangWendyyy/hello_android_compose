fb957f9a47a8f5a777d08fbb3d8805a4bdfeba3f
JiangWendyyy <j16698507162@163.com>
2024-04-10 14:17:01 +0800

    refactor

--- app/src/main/java/com/thoughtworks/androidtrain/SharedPreferenceActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/SharedPreferenceActivity.kt
@@ -8,36 +8,33 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SharedPreferenceActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
-    private val PREFERENCES_NAME = "MyPrefs"
-    private val IS_HINT_SHOWN_KEY = "isHintShown"
+    companion object {
+        const val PREFERENCES_NAME = "MyPrefs"
+        const val IS_HINT_SHOWN_KEY = "isHintShown"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         sharedPreferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE)
 
-        val isHintShown = sharedPreferences.getBoolean(IS_HINT_SHOWN_KEY, false)
+        val isHintShown = sharedPreferences.getBoolean(IS_HINT_SHOWN_KEY, true)
         if (isHintShown) {
-            setContentView(R.layout.shared_preference_layout_back)
-        } else {
-            setContentView(R.layout.shared_preference_layout_remind)
-            // 初始化 layout1 的按钮点击事件
-        }
-
-        if (!isHintShown) {
             setContentView(R.layout.shared_preference_layout_remind)
             val buttonShowHint = findViewById<Button>(R.id.know)
             buttonShowHint.setOnClickListener {
                 // 更新 SharedPreferences 并重新加载布局
                 updateSharedPreferencesAndReloadLayout()
             }
+        } else {
+            setContentView(R.layout.shared_preference_layout_back)
+            // 初始化 layout1 的按钮点击事件
         }
-
 
     }
     private fun updateSharedPreferencesAndReloadLayout() {
         // 保存新的 isHintShown 状态
         val editor = sharedPreferences.edit()
-        editor.putBoolean(IS_HINT_SHOWN_KEY, true)
+        editor.putBoolean(IS_HINT_SHOWN_KEY, false)
         editor.apply()
 
         // 重新加载布局
