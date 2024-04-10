6bfccfc7730d6f2f2c8f3f09b701dd395fd72e2d
JiangWendyyy <j16698507162@163.com>
2024-04-10 20:55:56 +0800

    remove unused import

--- app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
@@ -2,7 +2,6 @@ package com.thoughtworks.androidtrain
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
