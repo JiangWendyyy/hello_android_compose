275ba9f2644a5a5373761dc0267041414e5396c2
JiangWendyyy <j16698507162@163.com>
2024-04-14 16:00:51 +0800

    remove jsonUtil

--- app/src/main/java/com/thoughtworks/androidtrain/data/dao/SenderDao.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/data/dao/SenderDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import com.thoughtworks.androidtrain.entity.Comment
 import com.thoughtworks.androidtrain.entity.Sender
 
 @Dao
